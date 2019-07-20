b5d194cafd8845dfa22b970c9e725caa32cf3faa
Arpit Gupta <arpit@blueberrylabs.com>
2019-07-20 18:08:03 +0530

    updating policy

--- config/policies.js
+++ config/policies.js
@@ -13,11 +13,24 @@ module.exports.policies = {
     'show': ['is-logged-in']
   },
 
+  CategoryPageController: {
+    'getCategoriesByType': ['is-logged-in'],
+    'getCategoryDetail': ['is-logged-in'],
+    'checkAvailability': ['is-logged-in'],
+    'bookTicket': ['is-logged-in'],
+  },
+
   AdminController: {
     'showWorkPage': ['is-admin'],
     'showAllTheatres': ['is-admin'],
     'showAllCategories': ['is-admin'],
     'addNewTheatre': ['is-admin'],
     'addNewMovie': ['is-admin'],
+    'editTheatreDetail': ['is-admin'],
+    'updateTheatreDetail': ['is-admin'],
+    'editCategoryDetail': ['is-admin'],
+    'updateCategoryDetail': ['is-admin'],
+    'deleteTheatre': ['is-admin'],
+    'deleteCategory': ['is-admin']
   },
 };
