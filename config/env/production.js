15d6dcff0da45af84ff4f7a9fdead28d398190c1
Arpit Gupta <arpit@blueberrylabs.com>
2019-07-20 16:44:45 +0530

    dummy commit

--- config/env/production.js
+++ config/env/production.js
@@ -19,6 +19,30 @@
  * https://sailsjs.com/docs/concepts/deployment
  */
 
+var sessionOptions = {
+  name: 'test',
+
+  // user: 'root',
+  // password: 'Gmail@123',
+  // database: 'Ticket_Booking',
+  // host: '127.0.0.1',
+  // port: 3306,
+
+  user: 'root',
+  password: 'Gmail123',
+  database: 'Ticket_Booking',
+  host: 'database-1.coh0hs2zwmxd.us-east-2.rds.amazonaws.com',
+  port: 3306,
+
+  secret: 'eg[isfd-8yF9-7w2315df',
+
+  cookie: {
+    httpOnly: true,
+    maxAge: 2 * 365 * 24 * 60 * 60 * 1000, // 1 year
+    secure: false
+  },
+};
+
 module.exports = {
 
 
@@ -71,6 +95,19 @@ module.exports = {
        *                                                                           *
        ****************************************************************************/
       // ssl: true,
+
+      default: {
+        user: sessionOptions.user,
+        password: sessionOptions.password,
+        database: sessionOptions.database,
+        dialect: 'mysql',
+        options: {
+          dialect: 'mysql',
+          host: sessionOptions.host,
+          port: sessionOptions.port,
+          logging: true
+        }
+      }
 
     },
 
@@ -148,9 +185,10 @@ module.exports = {
      *                                                                          *
      ***************************************************************************/
     cors: {
-      // allowOrigins: [
-      //   'https://example.com',
-      // ]
+      allowOrigins: '*',
+      allRoutes: true,
+      allowRequestHeaders: 'Content-Type, Authorization',
+      allowCredentials: false
     },
 
   },
@@ -220,8 +258,10 @@ module.exports = {
      * https://sailsjs.com/config/session#?the-session-id-cookie                *
      *                                                                          *
      ***************************************************************************/
+    secret: sessionOptions.secret,
+    name: sessionOptions.name,
     cookie: {
-      // secure: true,
+      secure: false,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
 
@@ -250,10 +290,11 @@ module.exports = {
      * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
      *                                                                          *
      ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'http://localhost:1337',
+      'http://localhost',
+      'http://bookmytic.tk',
+    ],
 
 
     /***************************************************************************
@@ -322,7 +363,56 @@ module.exports = {
      * (https://sailsjs.com/config/http)                                        *
      *                                                                          *
      ***************************************************************************/
-    // trustProxy: true,
+    trustProxy: true,
+    middleware: {
+
+      /***************************************************************************
+       *                                                                          *
+       * The order in which middleware should be run for HTTP requests.           *
+       * (This Sails app's routes are handled by the "router" middleware below.)  *
+       *                                                                          *
+       ***************************************************************************/
+
+      order: [
+        'cookieParser',
+        'session',
+        // 'passportInit',
+        // 'passportSession',
+        'bodyParser',
+        'compress',
+        'poweredBy',
+        'router',
+        'www',
+      ],
+
+      bodyParser: (function _configureBodyParser() {
+        var skipper = require('skipper');
+        var middlewareFn = skipper({
+          strict: true,
+          maxTimeToBuffer: 100000
+        });
+        // ... more Skipper options here ...
+        return middlewareFn;
+      })(),
+
+      session: (function _configureSession() {
+        var store = new MySQLSessionStore({
+          host: sessionOptions.host,
+          port: sessionOptions.port,
+          user: sessionOptions.user,
+          password: sessionOptions.password,
+          database: sessionOptions.database,
+          resave: sessionOptions.resave,
+          checkExpirationInterval: sessionOptions.checkExpirationInterval
+        });
+
+        sessionOptions.store = store;
+
+        return session(sessionOptions);
+      })(),
+
+    }
+
 
   },
 
